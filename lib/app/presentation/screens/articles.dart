7327de8b5c77721fad6abd77bda5b899463bf897
William Porto <william.porto@nave.rs>
2021-09-15 17:08:44 -0300

    feature(presentation): adding simple SliverAppBar with leading, title and actions

--- lib/app/presentation/screens/articles.dart
+++ lib/app/presentation/screens/articles.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nave_articles/app/presentation/widgets/blurred_container.dart';
 import 'package:nave_articles/app/presentation/widgets/svg_icon.dart';
 
 class ArticlesScreen extends StatelessWidget {
@@ -9,18 +13,51 @@ class ArticlesScreen extends StatelessWidget {
   const ArticlesScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('ArticlesScreen'),
-              SvgIcon(
-                icon: AssetIcon.rocket,
-                color: Theme.of(context).colorScheme.primary,
-              )
-            ],
-          ),
+  Widget build(BuildContext context) => const Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            _SliverArticlesAppBar(),
+          ],
         ),
       );
+}
+
+class _SliverArticlesAppBar extends StatelessWidget {
+  const _SliverArticlesAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle? _headline6 = Theme.of(context).textTheme.headline6;
+    final TextStyle _titleStyle =
+        GoogleFonts.notoSans(fontWeight: FontWeight.bold)
+            .copyWith(fontStyle: _headline6?.fontStyle);
+    final ColorScheme _colorScheme = Theme.of(context).colorScheme;
+
+    return SliverAppBar(
+      centerTitle: false,
+      floating: true,
+      backgroundColor: Colors.transparent,
+      flexibleSpace: BlurredContainer(
+        sigma: 2.25,
+        opacity: .5,
+        color: _colorScheme.primary,
+      ),
+      leading: IconButton(
+        onPressed: () {},
+        icon: const SvgIcon(
+          icon: AssetIcon.rocket,
+        ),
+      ),
+      title: Text(
+        'Articles',
+        style: _titleStyle,
+      ),
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.bookmarks),
+        )
+      ],
+    );
+  }
 }
