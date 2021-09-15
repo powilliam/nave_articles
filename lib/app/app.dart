fecd6530046a712a4427c9df67abca3a8b6bd770
William Porto <william.porto@nave.rs>
2021-09-15 15:19:49 -0300

    chore(presentation): creating blank screens with its tests

---
+++ lib/app/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:nave_articles/app/presentation/screens/articles.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const MaterialApp(
+        home: ArticlesScreen(),
+      );
+}
