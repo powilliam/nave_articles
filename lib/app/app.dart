b541ad3995985234a557edc1ca9418b8a6729d4c
William Porto <william.porto@nave.rs>
2021-09-15 16:12:26 -0300

    chore(themes): adding primary and secondary colors and supporting only light variant

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:nave_articles/app/presentation/screens/articles.dart';
+import 'package:nave_articles/app/presentation/themes/default.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        theme: ThemeData.light(),
-        darkTheme: ThemeData.dark(),
+        theme: ThemeData.light().withColors(),
         home: const ArticlesScreen(),
       );
 }
--- lib/app/presentation/themes/default.dart
+++ lib/app/presentation/themes/default.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
-extension DefaultThemeExtension on ThemeData {}
+extension DefaultThemeExtension on ThemeData {
+  Color _primaryColor() => const Color(0xFF6600CA);
+
+  Color _secondaryColor() => const Color(0xFF00EAA1);
+
+  ThemeData withColors() => copyWith(
+      primaryColor: _primaryColor(),
+      colorScheme: colorScheme.copyWith(
+          primary: _primaryColor(), secondary: _secondaryColor()));
+}
