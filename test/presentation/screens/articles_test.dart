c45408660305c2c5ee3a82420b3872b11364ffb9
William Porto <william.porto@nave.rs>
2021-09-16 15:47:38 -0300

    refactor(tests): refactoring articles screen tests

--- test/presentation/screens/articles_test.dart
+++ test/presentation/screens/articles_test.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nave_articles/app/presentation/screens/articles.dart';
+import 'package:nave_articles/app/presentation/widgets/article.dart';
+import 'package:nave_articles/app/presentation/widgets/articles_list.dart';
+import 'package:nave_articles/app/presentation/widgets/category_filter.dart';
+import 'package:nave_articles/app/presentation/widgets/svg_icon.dart';
 
 void main() {
   testWidgets('Articles Screen should be able to render',
@@ -11,8 +15,28 @@ void main() {
       ),
     );
 
-    expect(find.text('ArticlesScreen'), findsOneWidget);
-    expect(find.byWidgetPredicate((final Widget widget) => widget is Scaffold),
-        findsOneWidget);
+    expect(find.text('Articles'), findsOneWidget);
+    expect(
+      find.byWidgetPredicate((final Widget widget) =>
+          widget is SvgIcon && widget.icon == AssetIcon.rocket),
+      findsOneWidget,
+    );
+    expect(
+      find.byWidgetPredicate(
+          (final Widget widget) => widget is SliverCategoryFilterList),
+      findsOneWidget,
+    );
+    expect(
+      find.byWidgetPredicate(
+        (final Widget widget) => widget is SliverArticlesList,
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.byWidgetPredicate(
+        (final Widget widget) => widget is ArticleCard,
+      ),
+      findsWidgets,
+    );
   });
 }
